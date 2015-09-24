a14157560730f7fbc60dc0b1af6e22fa848b1aaf
Michael Jungo <michael.jungo@unifr.ch>
2015-09-24 23:29:56 +0200

    add passwords controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'signup'},
+    :controllers => { :passwords => "passwords" }
   get 'main/index'
 
   devise_scope :user do
