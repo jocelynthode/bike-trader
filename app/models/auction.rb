0966b954aaa396f614796e27077086f0b2a247ff
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-09-26 13:12:19 +0200

    add belongs_to in auction model

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,8 +1,13 @@
 class Auction < ActiveRecord::Base
   has_many :bids
+  belongs_to :user
   validates_presence_of :start, :end
 
   validates_datetime :end, :after => :start
 
   mount_uploader :avatar, AvatarUploader
+
+  def self.user
+    User.find(@user_id)
+  end
 end
