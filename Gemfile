8fd3b04038282ac326abc8a1cc7a9b91fa55ed2f
Simon Brulhart <simon@brulhart.me>
2015-09-29 23:41:45 +0200

    switch to postgresql in every environment

--- Gemfile
+++ Gemfile
@@ -12,11 +12,8 @@ gem 'twitter-bootstrap-rails', '~> 3.2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
-# Use sqlite3 as the database for Active Record
-group :development, :test do
-  gem 'sqlite3'
-end
-group :production do
+# Use postgres as the database for Active Record
+group :production, :development, :test do
   gem 'pg'
 end
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150928084138) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "auctions", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
@@ -41,8 +44,8 @@ ActiveRecord::Schema.define(version: 20150928084138) do
     t.datetime "updated_at",                         null: false
   end
 
-  add_index "bids", ["auction_id"], name: "index_bids_on_auction_id"
-  add_index "bids", ["user_id"], name: "index_bids_on_user_id"
+  add_index "bids", ["auction_id"], name: "index_bids_on_auction_id", using: :btree
+  add_index "bids", ["user_id"], name: "index_bids_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -61,8 +64,10 @@ ActiveRecord::Schema.define(version: 20150928084138) do
     t.string   "address"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "bids", "auctions"
+  add_foreign_key "bids", "users"
 end
