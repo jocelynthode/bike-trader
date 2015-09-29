599f362aab969daecf17ff7e5a8f65a2b2052139
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-09-29 16:38:13 +0200

    add automatic bidding

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -13,13 +13,14 @@ class BidsController < ApplicationController
                      @auction.bids.select(&:persisted?).each.max_by(&:amount).amount
                    end
     info =  {:user => current_user, :time => DateTime.current}
+
     @bid = @auction.bids.build(info.merge(bid_params))
 
     if @bid.amount > current_bid
       if @bid.save
-        automatic_bid(@bid.amount)
-        if (DateTime.new.now + 5.minutes) > @auction.end
-          @auction.update(:end (DateTime.new.now + 15.minutes))
+        automatic_bid(@bid)
+        if (DateTime.now + 5.minutes) > @auction.end
+          @auction.update(end: (DateTime.now + 15.minutes))
         end
         redirect_to auction_path(@auction)
       else
@@ -36,13 +37,31 @@ class BidsController < ApplicationController
     def require_permission
       @auction = Auction.find(params[:auction_id])
       if @auction.ended? || current_user == @auction.user
-        redirect_to auction_path
+        redirect_to @auction
       end
     end
 
-    def automatic_bid(amount)
-      bids = Bid.where('threshold > ? AND auction_id = ?', amount, params[:auction_id])
+    def automatic_bid(bid)
+      @auction = Auction.find(params[:auction_id])
+      #find the highest bids that have a threshold higher than the current highest bid
+      bids = Bid.where('threshold > ? AND auction_id = ?', bid.amount, params[:auction_id]).having(
+          'MAX(amount)').group(:user_id).order(:threshold, time: :desc)
+      if bids.length > 1
+        #find the second to max threshold since this is the only one we need to beat
+        second_to_max = bids[-2].threshold
 
+        new_amount = bids[-1].threshold == second_to_max ? second_to_max : second_to_max + 1
+        if new_amount > bids[-1].threshold
+          new_amount = bids[-1].threshold
+        end
+
+      elsif bids.length == 1
+        new_amount = bids.first.threshold < amount + 1 ? bids.first.threshold : amount + 1
+      end
+      new_bid = @auction.bids.build(user_id: bids.first.user_id,
+                                    amount: new_amount, threshold: bid.threshold,
+                                    time: DateTime.now)
+      new_bid.save
     end
 
     def bid_params
